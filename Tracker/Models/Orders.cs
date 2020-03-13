6a6c607b8941432b82ac24b43f23b310dee34067
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-13 13:21:07 -0700

    add basic and initial code for order controller

--- Tracker/Models/Orders.cs
+++ Tracker/Models/Orders.cs
@@ -1,23 +1,24 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-// namespace Tracker.Models
-// {
-//   public class Order
-//   {
-//     public string Title;
-//     public string Date;
-//     public string Details;
-//     public int Price;
-//     public static List<Order> _orderInstance = new List<Order> {};
+namespace Tracker.Models
+{
+  public class Order
+  {
+    public string Title;
+    public string Date;
+    public string Details;
+    public int Price;
+    public static List<Order> _orderInstance = new List<Order> {};
 
-//     public Order(string title, string date, string details, int price)
-//     {
-//       Title = title; 
-//       Date = date;
-//       Details = details;
-//       Price = 0;
-//     }
-//   }
+    public Order(string title, string date, string details, int price)
+    {
+      Title = title; 
+      Date = date;
+      Details = details;
+      Price = 0;
+    }
+    
+  }
 
-// }
+}
