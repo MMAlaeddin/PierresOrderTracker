094a489e9b8e9a7d16c394be6fdd3af685e5a6c7
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-13 09:18:15 -0700

    create vendor class and construct

---
+++ Tracker/Models/Vendors.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tracker.Models
+{
+  public class Vendor
+  {
+    public string Business { get; set; }
+    public string Buyer { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> {};
+
+    public Vendor(string business, string buyer, string description)
+    {
+      Business = business;
+      Buyer = buyer;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+  }
+}
