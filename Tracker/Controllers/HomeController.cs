35114bffc0a3608aaadee59f9cecf3edf526ecb5
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-13 08:39:28 -0700

    add home controller route

---
+++ Tracker/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Tracker.Models;
+
+namespace Tracker.Controllers
+
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
